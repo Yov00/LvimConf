017216e9e42d2b14a053590334816b827065b256
Yov00 <yovelincho@gmail.com>
2025-10-05 18:04:15 +0300

    DOTNET lsp!

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,4 +2,11 @@ return {
   "marko-cerovac/material.nvim",
   "olimorris/onedarkpro.nvim",
   "shaunsingh/nord.nvim",
+  "sainnhe/everforest",
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest"
+    }
+  }
 }
